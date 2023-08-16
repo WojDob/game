55a0991b22d6b85f5e9136dfa28cb5860d49ad5a
wojdob <wojciech.dobrychlop@gmail.com>
2023-08-16 14:38:28 +0200

    Add collisions

--- main.c
+++ main.c
@@ -1,46 +1,49 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct
+{
 	int xPosition;
 	int yPosition;
 	int health;
 } Player;
 
 int screenSetUp();
-int mapSetUp(); 
+int mapSetUp();
 
-Player * playerSetUp();
+Player *playerSetUp();
 
-int handleInput(int input, Player * player);
-int playerMove(int y, int x, Player * player);
+int handleInput(int input, Player *player);
+int checkPosition(int newY, int newX, Player *player);
+
+int playerMove(int y, int x, Player *player);
 
 int main()
 {
-	Player * player;
+	Player *player;
 	int ch;
 	screenSetUp();
-   	mapSetUp(); 
+	mapSetUp();
 
 	player = playerSetUp();
 
-	while((ch =getch()) != 'q')
+	while ((ch = getch()) != 'q')
 	{
-		handleInput(ch, player);	
+		handleInput(ch, player);
 	}
 
-    	endwin();
-    	return 0;
+	endwin();
+	return 0;
 }
 
 int screenSetUp()
 {
-    	initscr();
-    	printw("Hello world!");
-    	noecho();
-    	refresh();
+	initscr();
+	printw("Hello world!");
+	noecho();
+	refresh();
 
-    	return 1;
+	return 1;
 }
 
 int mapSetUp()
@@ -51,7 +54,7 @@ int mapSetUp()
 	mvprintw(16, 13, "|......|");
 	mvprintw(17, 13, "|......|");
 	mvprintw(18, 13, "--------");
-	
+
 	mvprintw(2, 40, "--------");
 	mvprintw(3, 40, "|......|");
 	mvprintw(4, 40, "|......|");
@@ -60,47 +63,69 @@ int mapSetUp()
 	mvprintw(7, 40, "--------");
 }
 
-
-Player * playerSetUp()
+Player *playerSetUp()
 {
-	Player * newPlayer;
+	Player *newPlayer;
 	newPlayer = malloc(sizeof(Player));
 	newPlayer->xPosition = 14;
 	newPlayer->yPosition = 14;
-	newPlayer->health= 20;
-	mvprintw(newPlayer->yPosition,newPlayer->xPosition, "@");  
-	move(newPlayer->yPosition,newPlayer->xPosition);  
+	newPlayer->health = 20;
+	playerMove(14, 14, newPlayer);
+
 	return newPlayer;
 }
 
-
-int handleInput(int input, Player * player)
+int handleInput(int input, Player *player)
 {
-	switch(input)
+	int newY;
+	int newX;
+	switch (input)
 	{
-		case 'w':
-			playerMove(player->yPosition-1,player->xPosition,player);
-			break;
-		case 's':
-			playerMove(player->yPosition+1,player->xPosition,player);
-			break;
-		case 'a':
-			playerMove(player->yPosition,player->xPosition-1,player);
-			break;
-		case 'd':
-			playerMove(player->yPosition,player->xPosition+1,player);
-			break;
-		default:
-			break;
+	case 'W':
+	case 'w':
+		newY = player->yPosition - 1;
+		newX = player->xPosition;
+		break;
+	case 'S':
+	case 's':
+		newY = player->yPosition + 1;
+		newX = player->xPosition;
+		break;
+	case 'A':
+	case 'a':
+		newY = player->yPosition;
+		newX = player->xPosition - 1;
+		break;
+	case 'D':
+	case 'd':
+		newY = player->yPosition;
+		newX = player->xPosition + 1;
+		break;
+	default:
+		break;
+	}
+	checkPosition(newY, newX, player);
+}
+
+int checkPosition(int newY, int newX, Player *player)
+{
+	int space;
+	switch (mvinch(newY, newX))
+	{
+	case '.':
+		playerMove(newY, newX, player);
+		break;
+	default:
+		move(player->yPosition, player->xPosition);
+		break;
 	}
 }
 
-int playerMove(int y, int x, Player * player)
+int playerMove(int y, int x, Player *player)
 {
-	mvprintw(player->yPosition,player->xPosition, ".");  
+	mvprintw(player->yPosition, player->xPosition, ".");
 	player->yPosition = y;
 	player->xPosition = x;
-	mvprintw(player->yPosition,player->xPosition, "@");  
-	move(player->yPosition,player->xPosition);  
+	mvprintw(player->yPosition, player->xPosition, "@");
+	move(player->yPosition, player->xPosition);
 }
-
